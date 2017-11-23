4d230e640da29f2782ea9d43009b96b69febd8c4
finolex <hermitdudz@gmail.com>
2017-11-23 14:29:00 -0500

    {

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function dropdowntoggle {
-	var x = document.getElementsByClassName("hide");
-
-	if(x.style.display == 'none'){
-		x.style.display = 'block';
-	} else {
-		x.style.display = 'none';
-	}
-}
